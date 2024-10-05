4f72bc0256a8640342a88b5141da3fc678c9c55c
Najib <52837430+najchris11@users.noreply.github.com>
2024-10-05 00:47:08 -0400

    welp uhm i didn't succeed in applying the cool bg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,70 +11,6 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
 
-const projects: { title: string; href: string; description: string }[] = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-        title: "Hover Card",
-        href: "/docs/primitives/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-    },
-    {
-        title: "Progress",
-        href: "/docs/primitives/progress",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-]
-
-const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    )
-})
-ListItem.displayName = "ListItem"
-
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
@@ -110,25 +46,20 @@ export const Navbar = () => {
 
                         <NavigationMenuItem>
                             <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                                <Link href="/contact">Contact</Link>
+                                <Link href="/projects">Projects</Link>
                             </NavigationMenuLink>
                         </NavigationMenuItem>
 
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                                    {projects.map((project) => (
-                                        <ListItem
-                                            key={project.title}
-                                            title={project.title}
-                                            href={project.href}
-                                        >
-                                            {project.description}
-                                        </ListItem>
-                                    ))}
-                                </ul>
-                            </NavigationMenuContent>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                                <Link href="/resume">Resume</Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                                <Link href="/contact">Contact</Link>
+                            </NavigationMenuLink>
                         </NavigationMenuItem>
 
                     </NavigationMenuList>
@@ -136,7 +67,7 @@ export const Navbar = () => {
 
                 {/* Social Icons and Mode Toggle */}
                 <div className="flex items-center space-x-2">
-                    <div className="h-6 border-l border-green-600" /> {/* let's find a way to make this match the theme color*/}
+                    <div className="h-6 border-l border-primary" />
                     <ModeToggle />
                     <Button variant="outline" size="icon">
                         <Link href="https://github.com/najchris11" target="_blank" rel="noopener noreferrer">
@@ -153,53 +84,49 @@ export const Navbar = () => {
 
             {/* Mobile Menu */}
             {menuOpen &&
-            <div className="md:hidden mt-2">
-            <NavigationMenu>
-              <NavigationMenuList className="flex flex-col space-y-2">
-                {/* Mobile Navigation Items */}
-                <NavigationMenuItem>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                    <Link href="/" onClick={() => setMenuOpen(false)}>
-                      Home
-                    </Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-  
-                <NavigationMenuItem>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                    <Link href="/about" onClick={() => setMenuOpen(false)}>
-                      About
-                    </Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-  
-                <NavigationMenuItem>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
-                    <Link href="/contact" onClick={() => setMenuOpen(false)}>
-                      Contact
-                    </Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-  
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className="grid gap-3 p-4">
-                      {projects.map((project) => (
-                        <ListItem
-                          key={project.title}
-                          title={project.title}
-                          href={project.href}
-                        >
-                          {project.description}
-                        </ListItem>
-                      ))}
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
+                <div className="md:hidden mt-2">
+                    <NavigationMenu>
+                        <NavigationMenuList className="flex flex-col space-y-2">
+                            {/* Mobile Navigation Items */}
+                            <NavigationMenuItem>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                                    <Link href="/" onClick={() => setMenuOpen(false)}>
+                                        Home
+                                    </Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+
+                            <NavigationMenuItem>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                                    <Link href="/about" onClick={() => setMenuOpen(false)}>
+                                        About
+                                    </Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+
+                            <NavigationMenuItem>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                                    <Link href="/projects">Projects</Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+
+                            <NavigationMenuItem>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                                    <Link href="/resume">Resume</Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+
+                            <NavigationMenuItem>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()} asChild>
+                                    <Link href="/contact" onClick={() => setMenuOpen(false)}>
+                                        Contact
+                                    </Link>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
             }
         </nav>
 
